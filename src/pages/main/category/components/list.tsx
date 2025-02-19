acb16d2b74520eb146e40c2da66df4f83b1ca645
Scorti-Paul <scortip@gmail.com>
2025-02-19 06:30:22 +0000

    list category component

---
+++ src/pages/main/category/components/list.tsx
@@ -0,0 +1,50 @@
+import { GiftIcon, TrashIcon } from "@heroicons/react/24/outline";
+import { deleteCategory } from "api/mutations/category";
+import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
+
+export default function List({ data }: any) {
+  const [,setLoading] = useState(false);
+    const [category, setCategory] = useState([])
+  
+    useEffect(() => {
+      setCategory(data)
+    }, [data])
+  
+    const invokeDeleteCategory = (itemId: string) => {
+      setLoading(true);
+      deleteCategory(itemId)
+        .then((response) => {
+          setLoading(false);
+          toast.success(response?.message);
+          setCategory(category.filter((category: any) => category._id !== itemId))
+        })
+        .catch((error) => {
+          toast.error(error?.response?.data?.message);
+          setLoading(false);
+        });
+    };
+  return (
+    <>
+      {category?.map((item: any) => (
+        <div
+          key={item._id}
+          className="relative cat-card flex items-center space-x-3 rounded-lg border border-gray-300 bg-white px-6 py-5 shadow-sm focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:border-gray-400"
+        >
+          <div className="flex-shrink-0">
+            <GiftIcon className="h-10 w-10" />
+          </div>
+          <div className="min-w-0 flex-1">
+            <div className="focus:outline-none">
+              {/* <span className="absolute inset-0" aria-hidden="true" /> */}
+              <p className="text-sm font-medium text-gray-900">{item.name}</p>
+            </div>
+          </div>
+          <div className="remove-btn cursor-pointer" onClick={() => invokeDeleteCategory(item._id)}>
+            <TrashIcon className="h-6  w-6 text-red-600 bg-red-100 rounded-full p-1" />
+          </div>
+        </div>
+      ))}
+    </>
+  );
+}
