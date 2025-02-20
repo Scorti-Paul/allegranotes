1d389ec30878b639720357b20b3ec169b2424963
Scorti-Paul <scortip@gmail.com>
2025-02-20 11:08:19 +0000

    fix error

--- src/assets/navhr/index.tsx
+++ src/assets/navhr/index.tsx
@@ -7,9 +7,9 @@ const NavHr = () => {
         <path d="M0 0.5H233.25" stroke="url(#paint0_linear_4_1592)" />
         <defs>
           <linearGradient id="paint0_linear_4_1592" x1="0" y1="0.5" x2="231" y2="0.5" gradientUnits="userSpaceOnUse">
-            <stop stopColor="#E0E1E2" stop-opacity="0" />
+            <stop stopColor="#E0E1E2" stopOpacity="0" />
             <stop offset="0.5" stopColor="#E0E1E2" />
-            <stop offset="1" stopColor="#E0E1E2" stop-opacity="0.15625" />
+            <stop offset="1" stopColor="#E0E1E2" stopOpacity="0.15625" />
           </linearGradient>
         </defs>
       </svg>
