ee53be543bc41a81becad9e84c54634c086c41fe
Scorti-Paul <scortip@gmail.com>
2025-02-20 11:28:04 +0000

    fix create tag endpoint

--- src/api/mutations/tag/index.ts
+++ src/api/mutations/tag/index.ts
@@ -1,9 +1,9 @@
 import { post, del } from "api";
 
 export const createTag = async (body: any) => {
-  return await post("tag/create", { ...body });
+  return await post("tag", { ...body });
 };
 
 export const deleteTag = async (id: string) => {
-  return await del(`tag/delete/${id}`, );
+  return await del(`tag/${id}`, );
 };
