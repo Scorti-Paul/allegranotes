db25e928f53047e9af22202eb808cbd63fa971d8
Scorti-Paul <scortip@gmail.com>
2025-02-21 13:57:36 +0000

    add filter endpoint

--- src/api/mutations/notes/index.ts
+++ src/api/mutations/notes/index.ts
@@ -12,6 +12,10 @@ export const singleNote = async (id: any) => {
   return await get(`note/${id}`);
 };
 
+export const filterNote = async (category: any, tag: any) => {
+  return await get(`notes/filter/${category}/${tag}`);
+};
+
 export const deleteNote = async (id: any) => {
   return await del(`note/${id}`);
 };
